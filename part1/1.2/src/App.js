de1af4334f9d628019a0e541a15a9628a0381e7f
Elyorbek <edjalalov@outlook.comk>
2021-07-14 19:05:49 +0500

    Merge branch 'main' of https://github.com/Djalalov/Full-Stack-open-2021 into main

---
+++ part1/1.2/src/App.js
@@ -0,0 +1,71 @@
+import React from "react";
+
+const App = () => {
+  const course = "Half Stack application development";
+
+  const parts = [
+    {
+      name: "Fundamentals of React",
+      exercises: 10,
+    },
+    {
+      name: "Using props to pass data",
+      exercises: 7,
+    },
+    {
+      name: "State of a component",
+      exercises: 14,
+    },
+  ];
+
+  return (
+    <div>
+      <Header title={course} />
+      <Content parts={parts} />
+      <Total parts={parts} />
+    </div>
+  );
+};
+
+const Header = (props) => {
+  return <h1>{props.title}</h1>;
+};
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part
+        part={props.parts[0]["name"]}
+        exercises={props.parts[0]["exercises"]}
+      />
+      <Part
+        part={props.parts[1]["name"]}
+        exercises={props.parts[1]["exercises"]}
+      />
+      <Part
+        part={props.parts[2]["name"]}
+        exercises={props.parts[2]["exercises"]}
+      />
+    </div>
+  );
+};
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part} {props.exercises}
+    </p>
+  );
+};
+
+const Total = (props) => {
+  return (
+    <p>
+      Number of exercises{" "}
+      {props.parts[0]["exercises"] +
+        props.parts[1]["exercises"] +
+        props.parts[2]["exercises"]}
+    </p>
+  );
+};
+export default App;
